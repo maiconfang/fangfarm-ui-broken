e9a44cb77ee5ad00e5eb9c5268c462dfd9d8966b
Maicon fang <maicon.fang@fh.com.br>
2022-02-03 11:08:12 -0300

    Heroku - config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiURL: 'http://localhost:8080',
   basicAuth: btoa('maiffarm-web:web123')
 };
